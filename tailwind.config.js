0acfa071293541b9b9e28d896892051cf2b801b7
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-09-05 02:03:26 +0530

    feat: login and dashboard added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html',
+    "./templates/**/*.html",
+    "./static/js/**/*.js",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
-      }
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms')({
-      strategy: "base"
+    require("@tailwindcss/forms")({
+      strategy: "base",
     }),
+    require("flowbite/plugin"),
   ],
-}
-
+};
