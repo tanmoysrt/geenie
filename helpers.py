f536299e6ccf91fb92f817523804aab0673aeee3
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-09-14 23:39:00 +0530

    chore: added JWT_SECRET_KEY to env

--- helpers.py
+++ helpers.py
@@ -7,14 +7,14 @@ from app import app
 # Generate JWT token
 def generate_jwt(user_id):
     payload = {"user_id": user_id}
-    jwt_token = jwt.encode(payload, "secret_key", algorithm="HS256")
+    jwt_token = jwt.encode(payload, os.getenv("JWT_SECRET_KEY"), algorithm="HS256")
     return jwt_token
 
 
 # Verify JWT token
 def verify_jwt(jwt_token):
     try:
-        payload = jwt.decode(jwt_token, "secret_key", algorithms=["HS256"])
+        payload = jwt.decode(jwt_token, os.getenv("JWT_SECRET_KEY"), algorithms=["HS256"])
         user_id = payload["user_id"]
         return user_id
     except jwt.ExpiredSignatureError:
